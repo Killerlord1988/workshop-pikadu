ecf55b8ac00003c31c4024a38380aabf95930364
Никита <killerlord1988@gmail.com>
2020-11-27 18:38:08 +0400

    add autorization js

--- js/main.js
+++ js/main.js
@@ -2,10 +2,99 @@
 let menuToggle = document.querySelector('#menu-toggle');
 // Создаем переменную, в которую положим меню
 let menu = document.querySelector('.sidebar');
-// отслеживаем клик по кнопке меню и запускаем функцию 
+// отслеживаем клик по кнопке меню и запускаем функцию
 menuToggle.addEventListener('click', function (event) {
   // отменяем стандартное поведение ссылки
   event.preventDefault();
-  // вешаем класс на меню, когда кликнули по кнопке меню 
+  // вешаем класс на меню, когда кликнули по кнопке меню
   menu.classList.toggle('visible');
 })
+
+const loginElem = document.querySelector('.login');
+const loginForm = loginElem.querySelector('.login-form');
+const emailInput = loginElem.querySelector('.login-email');
+const passwordInput = loginElem.querySelector('.login-password');
+const loginSignup = loginElem.querySelector('.login-signup');
+
+const userElem = document.querySelector('.user');
+const userNameElem = document.querySelector('.user-name');
+
+const listUsers = [
+  {
+    id: '01',
+    email: 'nikita@mail.com',
+    password: '12345',
+    displayName: 'NikitaJs',
+  },
+  {
+    id: '02',
+    email: 'kate@mail.com',
+    password: '123456',
+    displayName: 'KateJs&PHP',
+  },
+];
+
+const setUsers = {
+  user: null,
+  logIn(email, password, handler) {
+    const user = this.getUser(email);
+    if (user && user.password === password) {
+      this.authorizedUser(user);
+      handler();
+    } else {
+      alert('Пользователь с такими данными не найден')
+    }
+  },
+  logOut() {
+
+  },
+  signUp(email, password, handler) {
+    if (!this.getUser(email)) {
+      const user = {email, password, displayName: email}
+      listUsers.push(user);
+      this.authorizedUser(user);
+      handler();
+    } else {
+      alert('Пользователь с таким email уже зарегистрирован')
+    }
+    console.log(listUsers);
+  },
+  getUser(email) {
+    return listUsers.find(item => item.email === email)
+  },
+  authorizedUser(user) {
+    this.user = user;
+  },
+};
+
+const toggleAuthDom = () => {
+  const user = setUsers.user;
+
+  if (user) {
+    loginElem.style.display = 'none';
+    userElem.style.display = '';
+    userNameElem.textContent = user.displayName;
+  } else {
+    loginElem.style.display = '';
+    userElem.style.display = 'none';
+  }
+};
+
+loginForm.addEventListener('submit', evt => {
+  evt.preventDefault();
+
+  const emailValue = emailInput.value;
+  const passwordValue = passwordInput.value;
+
+  setUsers.logIn(emailValue, passwordValue, toggleAuthDom);
+});
+
+loginSignup.addEventListener('click', evt => {
+  evt.preventDefault();
+  const emailValue = emailInput.value;
+  const passwordValue = passwordInput.value;
+
+  setUsers.signUp(emailValue, passwordValue, toggleAuthDom);
+});
+
+toggleAuthDom();
